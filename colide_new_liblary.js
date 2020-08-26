24af86b6b870d4a13b87b28bf23680dbd95c1e69
shreyas2005 <69760488+shreyas2005@users.noreply.github.com>
2020-08-26 22:16:10 +0530

    Update colide_new_liblary.js

--- colide_new_liblary.js
+++ colide_new_liblary.js
@@ -3,7 +3,7 @@ function colide_liblary(object1,object2){
 //var object2=wall.x;
 var bullet_right=object2.x+object2.width;
 var wall_left=object1.x;
-if(bullet_right===wall_left){
+if(bullet_right>wall_left){
     object2.velocityX=0;
     return true;
 }
