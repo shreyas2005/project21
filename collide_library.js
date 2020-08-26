409e22284617d09453477d833466476985d655ae
shreyas2005 <69760488+shreyas2005@users.noreply.github.com>
2020-08-26 22:15:06 +0530

    Update collide_library.js

--- collide_library.js
+++ collide_library.js
@@ -1,5 +1,5 @@
 function colide(object1,object2){
-    if(object1.x-object2.x===object1.width/2+object2.width/2){
+    if(object1.x-object2.x<object1.width/2+object2.width/2){
         return true;
     }
     return false;
